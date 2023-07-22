eba50491fdbce0e077340ae4a845ae2075d81d49
sakiko21”$ git config --global user.email sakko21f@gmail.com <sakko.21f@gmail.com>
2023-07-22 12:32:32 +0900

    port番号修正

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ import cookieParser from 'cookie-parser';
 
 //expressを実行してappに代入
 const app = express();
-const PORT = 3000;
+const PORT =process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
